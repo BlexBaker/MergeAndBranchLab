3dd30646159729ea2c736cec713a6ae6c2c4a623
Faye Cheng <fayehcheng@gmail.com>
2016-02-08 14:43:34 -0800

    Update SoftwareEngineeringLab.rb

--- SoftwareEngineeringLab.rb
+++ SoftwareEngineeringLab.rb
@@ -2,4 +2,5 @@
 puts "My name is Bill and I added this line to this file."
 puts "My name is Captain Picard and I added this line to this file"
 puts "My name is Brenna and I added this line to this file"
+puts "My name is Faye and I added this line to this file"
 
